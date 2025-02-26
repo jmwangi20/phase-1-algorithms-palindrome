f089126386721ea03da43593e7723939b129fa34
John Kihugo Mwangi <johnmwangi13119@gmail.com>
2025-02-26 18:35:28 +0300

    check for palindrome

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let name = "";
+
+  for(let i = word.length - 1; i >= 0; i--){
+    name += word[i]
+    console.log(name)
+  }
+  if(name === word){
+    return true
+  }
+  else{
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  create a function names isPalindrome and give it a single parameter 
+
+  create an empty variable with let keyword so that we can git it the reverse string and allows us to change the values stored in it 
+  loop through the argument passed and print it from the reverse using i--;
+  check for equality between the reversed name and the argument passed in the function 
+  return true or false base on the equality
 */
 
 /*
